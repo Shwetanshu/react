b103df653f26d59495b2b1c1cc39c47486640693
Shwetanshu <shaktibxr@gmail.com>
2019-10-19 22:27:13 +0530

    adding skeleton

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Create a react Component
-const App = function() {
-    return <div>Hi There!!</div>;
+const App = () => {
+    const labelText = "Enter Name:";
+    const buttonText = { text: 'Click me'};
+
+    return (
+    <div>
+        <label className="label" htmlFor="name">
+            {labelText}
+        </label>
+        <input id="name" type="text" />
+        <button style={{ backgroundColor: 'blue', color: 'white'}}>
+            {buttonText.text}
+        </button>
+    </div>
+    );
 };
 
 // Take the react Component & show it on the screen
